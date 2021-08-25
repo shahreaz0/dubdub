d8d51d6a135d4e08df5c80ccad3b2a57ac48fb42
shahreaz <ashahreaz@gmail.com>
2021-08-25 19:19:54 +0600

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,6 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <p>Rick and Morty</p>
-
-      <div id="my-id"></div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Menu } from "antd";
 import "./Navbar.css";
 
 const Navbar = (props) => {
+	const [current, setCurrent] = useState("one");
+
+	const handleClick = (e) => {
+		console.log("click ", e);
+		setCurrent(e.key);
+	};
 	return (
 		<div>
-			<h1>navbar</h1>
+			<Menu
+				onClick={handleClick}
+				selectedKeys={[current]}
+				mode="horizontal"
+			>
+				<Menu.Item key="one">
+					<a href="#">Episodes</a>
+				</Menu.Item>
+				<Menu.Item key="two">
+					<a href="#">Characters</a>
+				</Menu.Item>
+				<Menu.Item key="three">
+					<a href="#">Location</a>
+				</Menu.Item>
+			</Menu>
 		</div>
 	);
 };
