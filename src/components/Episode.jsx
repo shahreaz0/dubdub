35969b63de48d6f45cc1a64723e425aabde1245d
shahreaz <ashahreaz@gmail.com>
2021-08-27 01:48:44 +0600

    Style Episode component

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { Row, Col, Card, Image, Descriptions, Divider, Avatar } from "antd";
+const { Meta } = Card;
+import "./Episode.css";
 
 const Episode = (props) => {
 	const { id } = props.match.params;
@@ -13,6 +17,7 @@ const Episode = (props) => {
 				characters {
 					name
 					image
+					id
 				}
 			}
 		}
@@ -31,13 +36,49 @@ const Episode = (props) => {
 		);
 	if (error) return <p>Error :(</p>;
 
-	const { name, air_date, episode } = data.episode;
+	const { name, air_date, episode, characters } = data.episode;
+	const characterCard = characters.map((character, key) => (
+		<Col xs={24} sm={8} lg={6} key={key}>
+			<Link to={`/characters/${character.id}`}>
+				<Card hoverable size="small">
+					<Meta
+						avatar={<Avatar src={character.image} />}
+						title={character.name}
+					/>
+				</Card>
+			</Link>
+		</Col>
+	));
 
 	return (
 		<div className="m-lg">
-			<h1>{name}</h1>
-			<p>{air_date}</p>
-			<p>{episode}</p>
+			<Row gutter={[16, 16]}>
+				<Col lg={8} md={8} sm={8} xs={24}>
+					<div className="card-area">
+						<h3 className="head">Name</h3>
+						<p className="text">{name}</p>
+					</div>
+				</Col>
+				<Col lg={8} md={8} sm={8} xs={24}>
+					<div className="card-area">
+						<h3 className="head">Air Date</h3>
+						<p className="text">{air_date}</p>
+					</div>
+				</Col>
+				<Col lg={8} md={8} sm={8} xs={24}>
+					<div className="card-area">
+						<h3 className="head">Episode</h3>
+						<p className="text">{episode}</p>
+					</div>
+				</Col>
+			</Row>
+
+			<Divider />
+			<b className="characters">Character's in this episode</b>
+
+			<Row gutter={[8, 8]} className="py-2">
+				{characterCard}
+			</Row>
 		</div>
 	);
 };
